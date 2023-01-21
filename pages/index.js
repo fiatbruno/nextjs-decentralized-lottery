819d26d67d7e4b35608894051aba33915b57bb66
Fiat Bruno <fiatbruno10@gmail.com>
2023-01-21 11:05:24 +0200

    Implementing the connect button using web3uikit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from "next/head"
 import Image from "next/image"
 import { Inter } from "@next/font/google"
 import styles from "@/styles/Home.module.css"
-import Header from "@/components/ManualHeader"
+// import Header from "@/components/ManualHeader"
+import Header from "@/components/Header"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,6 +16,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            Decentralized Lottery
             <Header />
             Hello!
             {/* <main className={styles.main}>
