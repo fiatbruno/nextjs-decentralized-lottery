fe1e6a4665fefb9d6fbfbee3c1a842ce71568d52
Fiat Bruno <fiatbruno10@gmail.com>
2023-01-21 12:52:39 +0200

    Added constants to lottery entrance

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -1,16 +1,37 @@
-import { useWeb3Contract } from "react-moralis" 
+import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants/index"
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
 
 export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
 
-    
-
-    // const { runContractFunction: enterRaffe } = useWeb3Contract(
-    //     abi:
-    //     contractAddress:
-    //     functionName:
+    // const { runContractFunction: enterRaffe } = useWeb3Contract({
+    //     abi: abi,
+    //     contractAddress: raffleAddress, // specify the networkId
+    //     functionName: "enterRaffle",
     //     params: {},
-    //     msgValue:
-    // )
+    //     msgValue: "",
+    // })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            // try to read the raffle entrance fee
+            async function updateUI() {
+                const entranceFeeFromContract = await getEntranceFee()
+            }
+            updateUI()
+        }
+    }, [isWeb3Enabled])
 
     return (
         <div>
